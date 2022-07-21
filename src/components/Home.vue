c4aea7d2f5c3370101bbf612442c5bebc49a4a44
Alex Hardman <alexhardman@launchdarkly.com>
2022-07-21 17:32:12 -0400

    initial commit with working project

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import Image from 'primevue/image';
+</script>
+
+<template>
+  <Image src="./logo.png" alt="LaunchDarkly Logo">
+    <template #indicator>
+        Preview Content
+    </template>
+  </Image>
+</template>
+
+<style>
+.greeting {
+  color: red;
+  font-weight: bold;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import PrimeVue from 'primevue/config'
+import Button from 'primevue/button';
+import Image from 'primevue/image';
+
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';  
+
+const app = createApp(App);
+app.use(PrimeVue);
+app.component('Button', Button);
+app.component('Button', Image);
+app.mount('#app');
